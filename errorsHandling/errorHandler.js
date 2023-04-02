e5f91af174711ac72f3d7936284b1c563d5cc593
Your Name <youremail@domain.com>
2023-04-02 13:29:58 +0100

    refactoring task 7

--- errorsHandling/errorHandler.js
+++ errorsHandling/errorHandler.js
@@ -11,10 +11,6 @@ function handle400Errors(err, req, res, next) {
 function psqlErrors(err, req, res, next) {
   if (err.code === "23503") {
     res.status(400).json({ error: "Invalid" });
-  } else if (err.code === "22P02") {
-    res.status(400).send({ message: "Invalid" });
-  } else if (err.message === 'Invalid column name') {
-    res.status(400).send({message: "Invalid column name"});
   } else {
     next(err);
   }
